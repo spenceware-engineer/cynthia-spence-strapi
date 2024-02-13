618166dc924ea2c82ed14a3143ef763f4eccf113
cynthiaspence <cynthia.spence@world50.com>
2024-02-13 00:49:49 -0500

    add back introspection pieces in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,10 @@ module.exports = ({ env }) => ({
       playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
+      introspection: true,
       apolloServer: {
         tracing: false,
+        introspection: true,
       },
     },
   },
