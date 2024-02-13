03199a000bfc2c8f5d7ad18c320d4ecec33c23d7
cynthiaspence <cynthia.spence@world50.com>
2024-02-12 23:52:05 -0500

    show graphql sandbox @ /graphql

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,27 @@
 module.exports = ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      introspection: true,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET_NAME'),
-          },
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
       },
       actionOptions: {
         upload: {
