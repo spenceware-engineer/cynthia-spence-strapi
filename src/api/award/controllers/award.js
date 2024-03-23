68f5aefcf01d18487eddd58d0f9116d183f797d7
Cynthia Spence <cynthia.spence.dev@gmail.com>
2024-03-22 21:36:27 -0400

    Update headers for Strapi

--- src/api/award/controllers/award.js
+++ src/api/award/controllers/award.js
@@ -1,9 +1,20 @@
-'use strict';
+'use strict'
 
 /**
  * award controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::award.award');
+module.exports = createCoreController(
+  'api::award.award',
+  ({ strapi }) => ({
+    setHeaders(ctx) {
+      ctx.response.headers = {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Controll-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH'
+      }
+    }
+  })
+)
--- src/api/my-info/controllers/my-info.js
+++ src/api/my-info/controllers/my-info.js
@@ -1,9 +1,20 @@
-'use strict';
+'use strict'
 
 /**
  * my-info controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::my-info.my-info');
+module.exports = createCoreController(
+  'api::my-info.my-info',
+  ({ strapi }) => ({
+    setHeaders(ctx) {
+      ctx.response.headers = {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Controll-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH'
+      }
+    }
+  })
+)
